3872a3479e81b18e18b9e20ad449672c6e81c4db
Suprunov Anton <suprunovvanton666@gmail.com>
2024-02-26 23:12:03 +0300

    Экран входа

---
+++ MVPproject/MVPproject/PersonData.swift
@@ -0,0 +1,10 @@
+// PersonData.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+
+/// Модель данных пользователя
+struct PersonData {
+    let email: String
+    let password: String
+}
