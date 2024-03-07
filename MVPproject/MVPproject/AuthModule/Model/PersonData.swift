706c20af523e0f79b9937dd56525c36494a09a96
Suprunov Anton <suprunovvanton666@gmail.com>
2024-03-07 17:35:16 +0300

    5 таск

--- MVPproject/MVPproject/AuthModule/Model/PersonData.swift
+++ MVPproject/MVPproject/AuthModule/Model/PersonData.swift
@@ -4,9 +4,37 @@
 import Foundation
 
 /// Модель данных пользователя
-struct PersonData {
+class PersonData: Codable {
+    private enum PersonDataKeys: String {
+        case email
+        case password
+    }
+
     // Email Пользователя
-    let email: String
+    private var email: String = ""
     // Пароль Пользователя
-    let password: String
+    private var password: String = ""
+
+    init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+
+    func encode(with coder: NSCoder) {
+        coder.encode(email, forKey: PersonDataKeys.email.rawValue)
+        coder.encode(password, forKey: PersonDataKeys.password.rawValue)
+    }
+
+    required init?(coder: NSCoder) {
+        email = coder.decodeObject(forKey: PersonDataKeys.email.rawValue) as? String ?? ""
+        password = coder.decodeObject(forKey: PersonDataKeys.password.rawValue) as? String ?? ""
+    }
+
+    func getPassword() -> String {
+        password
+    }
+
+    func getEmail() -> String {
+        email
+    }
 }
